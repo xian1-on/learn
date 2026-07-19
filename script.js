37ea5945b4085c5dcf1e7fb48ddf9749b6a65fc5
xbh <1686173860@qq.com>
2026-07-19 08:02:34 +0800

    这是第五次修改，修改了文案

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function changeText() {
-        document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
+        document.getElementById('msg').textContent = '祝你身体健康，天天开心！！！';
       }
